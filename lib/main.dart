d1f41cfb18534a2def0d00e000da7499255ef514
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-07 20:36:09 +0300

    problem not found yet

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Container(
-        child: Text("merhaba"),
+        child: loginPage(),
       ),
     );
   }
