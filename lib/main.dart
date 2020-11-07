4bf798c43dc13f492a85779a4470b60c5e1338b0
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-07 20:16:54 +0300

    add new screen called

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Sc
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text("selam"),
+        ),
+      ),
+
+    );
   }
 }
 
---
+++ lib/screns/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class loginPage extends StatefulWidget {
+  @override
+  _loginPageState createState() => _loginPageState();
+}
+
+class _loginPageState extends State<loginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Contan
+        ],
+      ),
+    );
+  }
+}
